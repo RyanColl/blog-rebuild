2bc8f023a8edc565e20fff41b64af543e5c6131c
Ryan Collicutt <ryancollicutt22@gmail.com>
2021-10-17 14:57:44 -0700

    big push

---
+++ src/Services/Services.tsx
@@ -0,0 +1,34 @@
+
+
+export const blogList = [
+    {
+        id: 1,
+        title: 'Build A Server-less Full Stack App With Google Firebase Functions',
+        imgUrl: '/img/firebase-logo-opacity-0.png'
+    },
+    {
+        id: 2,
+        title: 'Integrate Google Maps In A React Native Project',
+        imgUrl: '/img/maps.png'
+    },
+    {
+        id: 3,
+        title: 'How To Create A Parralax Effect in React In 4 Simple Steps',
+        imgUrl: '/img/web-stack-logo-cut.png'
+    }
+]
+
+export const Blogs = [
+    {
+        id: 1,
+        title: 'Build A Server-less Full Stack App With Google Firebase Functions',
+        summary: `Are you looking to make app-creation easier and more fun? In this blog post, I am going to cover Google Firebase's ability to host your app, and store your data, all from the client side. We will be building this app with React, so ensure you have nodeJS installed on your local computer, as well as a runtime environment like VSCode or WebStorm. We will be creating this project with Typescript too, so get prepared to learn a thing or two!`,
+        content: `Whether you are a novice or an experienced web-developer, having the right technologies and guidance make developing fun. That's why I want to bring to you a relatively new full stack application. It's… React, with... Firebase! Read on and I will explain how two stacks can be a full stack, and why we should go serverless for most of our apps. We are going to be using the JavaScript framework called React, and the hosting service called Firebase, however Firebase does a lot more than just host, it allows us to make server-less full stack applications with ease.Before we start, we will want to ensure we have a code editor installed, like VSCode . After that, we will want to ensure we have NodeJS installed on our machine. We can do that by visiting this link and installing the package. This also installs npm, which we will use for installing our react app. Finally, we can install yarn . Similar to npm, it helps us install packages, however this is optional.`,
+    },
+    {
+        id: 2,
+        title: 'Build A Server-less Full Stack App With Google Firebase Functions',
+        summary: `Are you looking to make app-creation easier and more fun? In this blog post, I am going to cover Google Firebase's ability to host your app, and store your data, all from the client side. We will be building this app with React, so ensure you have nodeJS installed on your local computer, as well as a runtime environment like VSCode or WebStorm. We will be creating this project with Typescript too, so get prepared to learn a thing or two!`,
+        content: `Whether you are a novice or an experienced web-developer, having the right technologies and guidance make developing fun. That's why I want to bring to you a relatively new full stack application. It's… React, with... Firebase! Read on and I will explain how two stacks can be a full stack, and why we should go serverless for most of our apps. We are going to be using the JavaScript framework called React, and the hosting service called Firebase, however Firebase does a lot more than just host, it allows us to make server-less full stack applications with ease.Before we start, we will want to ensure we have a code editor installed, like VSCode . After that, we will want to ensure we have NodeJS installed on our machine. We can do that by visiting this link and installing the package. This also installs npm, which we will use for installing our react app. Finally, we can install yarn . Similar to npm, it helps us install packages, however this is optional.`,
+    },
+]
